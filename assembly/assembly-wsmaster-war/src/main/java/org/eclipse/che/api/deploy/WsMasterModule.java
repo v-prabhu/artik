748545e69201edc63c468a48897e41b916233108
Yevhenii Voevodin <evoevodin@codenvy.com>
2016-07-06 09:48:09 +0300

    Adapt the code to UserService refactorings

--- assembly/assembly-wsmaster-war/src/main/java/org/eclipse/che/api/deploy/WsMasterModule.java
+++ assembly/assembly-wsmaster-war/src/main/java/org/eclipse/che/api/deploy/WsMasterModule.java
@@ -31,7 +31,8 @@ public class WsMasterModule extends AbstractModule {
         bind(org.eclipse.che.api.ssh.server.SshService.class);
         bind(org.eclipse.che.api.machine.server.recipe.RecipeService.class);
         bind(org.eclipse.che.api.user.server.UserService.class);
-        bind(org.eclipse.che.api.user.server.UserProfileService.class);
+        bind(org.eclipse.che.api.user.server.ProfileService.class);
+        bind(org.eclipse.che.api.user.server.PreferencesService.class);
         bind(org.eclipse.che.api.workspace.server.stack.StackLoader.class);
         bind(org.eclipse.che.api.workspace.server.stack.StackService.class);
         bind(org.eclipse.che.api.workspace.server.WorkspaceService.class);
