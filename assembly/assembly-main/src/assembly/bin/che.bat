4d761cd000998942e1e0d2ea2ef62bd849101f21
Mihail Kuznyetsov <mkuznyetsov@codenvy.com>
2017-01-04 14:17:47 +0200

    Set new license year

--- assembly/assembly-main/src/assembly/bin/che.bat
+++ assembly/assembly-main/src/assembly/bin/che.bat
@@ -1,5 +1,5 @@
 @REM
-@REM Copyright (c) 2016 Samsung Electronics Co., Ltd.
+@REM Copyright (c) 2016-2017 Samsung Electronics Co., Ltd.
 @REM All rights reserved. This program and the accompanying materials
 @REM are made available under the terms of the Eclipse Public License v1.0
 @REM which accompanies this distribution, and is available at
