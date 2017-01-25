0b46a3aa2293711e82e6ea68094792e2f6d2c43a
Anatoliy Bazko <abazko@codenvy.com>
2017-01-25 21:38:16 +0200

    CHE-3720: Decouple different agents on different maven modules (#296)

--- plugins/plugin-artik-server/src/main/java/org/eclipse/che/plugin/machine/artik/ArtikTerminalLauncher.java
+++ plugins/plugin-artik-server/src/main/java/org/eclipse/che/plugin/machine/artik/ArtikTerminalLauncher.java
@@ -11,13 +11,10 @@
  *******************************************************************************/
 package org.eclipse.che.plugin.machine.artik;
 
-import org.eclipse.che.api.agent.server.AgentRegistry;
-import org.eclipse.che.api.agent.server.exception.AgentException;
-import org.eclipse.che.api.agent.server.model.impl.AgentKeyImpl;
-import org.eclipse.che.api.agent.shared.model.Agent;
+import org.eclipse.che.api.agent.ExecAgent;
+import org.eclipse.che.api.agent.SshMachineExecAgentLauncher;
 import org.eclipse.che.api.core.ServerException;
 import org.eclipse.che.api.machine.server.spi.Instance;
-import org.eclipse.che.plugin.machine.ssh.SshMachineImplTerminalLauncher;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -27,33 +24,26 @@ import javax.inject.Named;
  *
  * @author Valeriy Svydenko
  */
-public class ArtikTerminalLauncher extends SshMachineImplTerminalLauncher {
+public class ArtikTerminalLauncher extends SshMachineExecAgentLauncher {
     public static final  String TERMINAL_LOCATION_PROPERTY       = "artik.terminal.location";
     private static final String ARTIK_TERMINAL_AGENT_RUN_COMMAND = "artik.terminal_agent.run_command";
     private static final String ARTIK_MACHINE_TYPE               = "artik";
     private static final long   TERMINAL_AGENT_MAX_START_TIME_MS = 120_000;
     private static final long   TERMINAL_AGENT_PING_DELAY_MS     = 2000;
 
-    private final AgentRegistry agentRegistry;
+    private final ExecAgent execAgent;
 
     @Inject
     public ArtikTerminalLauncher(@Named(TERMINAL_LOCATION_PROPERTY) String terminalLocation,
                                  @Named(ARTIK_TERMINAL_AGENT_RUN_COMMAND) String terminalRunCommand,
                                  ArtikDeviceTerminalFilesPathProvider terminalPathProvider,
-                                 AgentRegistry agentRegistry) {
+                                 ExecAgent execAgent) {
         super(TERMINAL_AGENT_MAX_START_TIME_MS, TERMINAL_AGENT_PING_DELAY_MS, terminalLocation, terminalRunCommand, terminalPathProvider);
-        this.agentRegistry = agentRegistry;
+        this.execAgent = execAgent;
     }
 
     public void launch(Instance machine) throws ServerException {
-        Agent agent;
-        try {
-            agent = agentRegistry.getAgent(AgentKeyImpl.parse("org.eclipse.che.terminal"));
-        } catch (AgentException e) {
-            throw new ServerException("org.eclipse.che.terminal agent not found");
-        }
-
-        super.launch(machine, agent);
+        super.launch(machine, execAgent);
     }
 
     @Override
