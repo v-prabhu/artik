7fd9922077d76fcf335e81c4d424bd7bf8275a2b
Valeriy Svydenko <vsvydenko@codenvy.com>
2017-02-10 17:31:03 +0200

    ARTIK-325: show error notification if the device is turned off while updating the sdk (#336)

--- plugins/plugin-artik-ide/src/main/java/org/eclipse/che/plugin/artik/ide/updatesdk/UpdateSDKPresenter.java
+++ plugins/plugin-artik-ide/src/main/java/org/eclipse/che/plugin/artik/ide/updatesdk/UpdateSDKPresenter.java
@@ -11,24 +11,19 @@
  *******************************************************************************/
 package org.eclipse.che.plugin.artik.ide.updatesdk;
 
-import com.google.gwt.core.client.JsArrayMixed;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
+import com.google.web.bindery.event.shared.EventBus;
 
 import org.eclipse.che.api.core.model.machine.Machine;
 import org.eclipse.che.api.core.model.workspace.WorkspaceStatus;
 import org.eclipse.che.api.machine.shared.dto.MachineDto;
 import org.eclipse.che.api.promises.client.Function;
-import org.eclipse.che.api.promises.client.FunctionException;
-import org.eclipse.che.api.promises.client.Operation;
-import org.eclipse.che.api.promises.client.OperationException;
 import org.eclipse.che.api.promises.client.Promise;
-import org.eclipse.che.api.promises.client.PromiseError;
 import org.eclipse.che.api.promises.client.js.Promises;
-import org.eclipse.che.api.workspace.shared.dto.WorkspaceDto;
 import org.eclipse.che.ide.api.app.AppContext;
 import org.eclipse.che.ide.api.dialogs.ConfirmCallback;
 import org.eclipse.che.ide.api.dialogs.ConfirmDialog;
@@ -36,6 +31,7 @@ import org.eclipse.che.ide.api.dialogs.DialogFactory;
 import org.eclipse.che.ide.api.notification.NotificationManager;
 import org.eclipse.che.ide.api.notification.StatusNotification;
 import org.eclipse.che.ide.api.workspace.WorkspaceServiceClient;
+import org.eclipse.che.ide.api.workspace.event.MachineStatusChangedEvent;
 import org.eclipse.che.ide.ui.loaders.request.LoaderFactory;
 import org.eclipse.che.ide.ui.loaders.request.MessageLoader;
 import org.eclipse.che.plugin.artik.ide.ArtikLocalizationConstant;
@@ -56,7 +52,7 @@ import static org.eclipse.che.ide.api.notification.StatusNotification.Status.PRO
  * @author Artem Zatsarynnyi
  */
 @Singleton
-public class UpdateSDKPresenter implements UpdateSDKView.ActionDelegate {
+public class UpdateSDKPresenter implements UpdateSDKView.ActionDelegate, MachineStatusChangedEvent.Handler {
 
     private final UpdateSDKView              view;
     private final AppContext                 appContext;
@@ -69,9 +65,12 @@ public class UpdateSDKPresenter implements UpdateSDKView.ActionDelegate {
     private final MessageLoader              loader;
     private final ArrayList<TargetForUpdate> targetsList;
 
+    private List<StatusNotification> notifications;
+
     @Inject
     public UpdateSDKPresenter(UpdateSDKView view,
                               AppContext appContext,
+                              EventBus eventBus,
                               DeviceServiceClient deviceServiceClient,
                               NotificationManager notificationManager,
                               Provider<SDKInstaller> sdkUpdaterProvider,
@@ -92,6 +91,9 @@ public class UpdateSDKPresenter implements UpdateSDKView.ActionDelegate {
         loader = loaderFactory.newLoader(localizationConstants.updateSDKViewLoaderMessage());
 
         targetsList = new ArrayList<>();
+        notifications = new ArrayList<>();
+
+        eventBus.addHandler(MachineStatusChangedEvent.TYPE, this);
     }
 
     /** Show the dialog. */
@@ -100,91 +102,66 @@ public class UpdateSDKPresenter implements UpdateSDKView.ActionDelegate {
 
         view.show();
         view.setTargets(targetsList);
-        view.setAvailableVersions(Collections.<String>emptyList());
+        view.setAvailableVersions(Collections.emptyList());
         view.setEnabledInstallButton(false);
 
         loader.show();
 
-        sdkUpdaterProvider.get().getAvailableSDKVersions().then(new Operation<List<String>>() {
-            @Override
-            public void apply(List<String> versions) throws OperationException {
-                view.setAvailableVersions(versions);
-                fillTargetsForUpdate();
-            }
-        }).catchError(new Operation<PromiseError>() {
-            @Override
-            public void apply(PromiseError arg) throws OperationException {
-                loader.hide();
+        sdkUpdaterProvider.get().getAvailableSDKVersions().then(versions -> {
+            view.setAvailableVersions(versions);
+            fillTargetsForUpdate();
+        }).catchError(arg -> {
+            loader.hide();
 
-                notificationManager.notify(localizationConstants.updateSDKNotificationGetVersionsTitle(),
-                                           localizationConstants.updateSDKNotificationGetVersionsFailMessage() + arg.getMessage(),
-                                           FAIL,
-                                           FLOAT_MODE);
-            }
+            notificationManager.notify(localizationConstants.updateSDKNotificationGetVersionsTitle(),
+                                       localizationConstants.updateSDKNotificationGetVersionsFailMessage() + arg.getMessage(),
+                                       FAIL,
+                                       FLOAT_MODE);
         });
     }
 
     private void fillTargetsForUpdate() {
         loader.show();
-        deviceServiceClient.getDevices().then(new Function<List<MachineDto>, List<TargetForUpdate>>() {
-            @Override
-            public List<TargetForUpdate> apply(List<MachineDto> machines) throws FunctionException {
-                List<TargetForUpdate> list = new ArrayList<>();
-                for (Machine machine : machines) {
-                    if (RUNNING.equals(machine.getStatus())) {
-                        list.add(new TargetForUpdate(machine.getId(), machine.getConfig().getName()));
-                    }
+        deviceServiceClient.getDevices().then((Function<List<MachineDto>, List<TargetForUpdate>>)machines -> {
+            List<TargetForUpdate> list = new ArrayList<>();
+            for (Machine machine : machines) {
+                if (RUNNING.equals(machine.getStatus())) {
+                    list.add(new TargetForUpdate(machine.getId(), machine.getConfig().getName()));
                 }
-                list.add(new TargetForUpdate(appContext.getDevMachine().getId(), "Workspace"));
-                return list;
             }
-        }).then(new Operation<List<TargetForUpdate>>() {
-            @Override
-            public void apply(List<TargetForUpdate> targets) throws OperationException {
-                final Promise<String>[] promises = (Promise<String>[])new Promise[targets.size()];
+            list.add(new TargetForUpdate(appContext.getDevMachine().getId(), "Workspace"));
+            return list;
+        }).then(targets -> {
+            final Promise<String>[] promises = (Promise<String>[])new Promise[targets.size()];
 
-                for (int i = 0; i < targets.size(); i++) {
-                    final TargetForUpdate target = targets.get(i);
-                    promises[i] = sdkUpdaterProvider.get()
-                                                    .getInstalledSDKVersion(target)
-                                                    .then(new Operation<String>() {
-                                                        @Override
-                                                        public void apply(String arg) throws OperationException {
-                                                            target.setCurrentVersion(arg);
-                                                            targetsList.add(target);
+            for (int i = 0; i < targets.size(); i++) {
+                final TargetForUpdate target = targets.get(i);
+                promises[i] = sdkUpdaterProvider.get()
+                                                .getInstalledSDKVersion(target)
+                                                .then(arg -> {
+                                                    target.setCurrentVersion(arg);
+                                                    targetsList.add(target);
+                                                    view.setTargets(targetsList);
+                                                    view.setEnabledInstallButton(!targetsList.isEmpty());
+                                                }).catchError(arg -> {
+                            target.setCurrentVersion("n/a");
+                            targetsList.add(target);
 
-                                                            view.setTargets(targetsList);
-                                                            view.setEnabledInstallButton(!targetsList.isEmpty());
-                                                        }
-                                                    }).catchError(new Operation<PromiseError>() {
-                                @Override
-                                public void apply(PromiseError arg) throws OperationException {
-                                    target.setCurrentVersion("n/a");
-                                    targetsList.add(target);
+                            view.setTargets(targetsList);
 
-                                    view.setTargets(targetsList);
-
-                                    notificationManager.notify(target.getName(),
-                                                               localizationConstants.updateSDKNotificationGetInstalledVersionFailMessage() +
-                                                               arg.getMessage(),
-                                                               FAIL,
-                                                               FLOAT_MODE);
-                                }
-                            });
-                }
-
-                Promises.all(promises).then(new Operation<JsArrayMixed>() {
-                    @Override
-                    public void apply(JsArrayMixed arg) throws OperationException {
-                        loader.hide();
-                    }
-                }).catchError(new Operation<PromiseError>() {
-                    @Override
-                    public void apply(PromiseError arg) throws OperationException {
-                        loader.hide();
-                    }
-                });
+                            notificationManager.notify(target.getName(),
+                                                       localizationConstants.updateSDKNotificationGetInstalledVersionFailMessage() +
+                                                       arg.getMessage(),
+                                                       FAIL,
+                                                       FLOAT_MODE);
+                        });
             }
+
+            Promises.all(promises).then(arg -> {
+                loader.hide();
+            }).catchError(arg -> {
+                loader.hide();
+            });
         });
     }
 
@@ -192,79 +169,80 @@ public class UpdateSDKPresenter implements UpdateSDKView.ActionDelegate {
     public void onInstallClicked() {
         Promise<String>[] promises = (Promise<String>[])new Promise[targetsList.size()];
         for (TargetForUpdate target : targetsList) {
-            promises[targetsList.indexOf(target)] = update(target);
+            StatusNotification notification = notificationManager.notify(target.getName(),
+                                                                         localizationConstants.updateSDKNotificationUpdatingMessage(),
+                                                                         PROGRESS,
+                                                                         FLOAT_MODE);
+            notifications.add(notification);
+            promises[targetsList.indexOf(target)] = update(target, notification);
         }
 
         view.close();
 
-        Promises.all(promises).then(new Operation<JsArrayMixed>() {
-            @Override
-            public void apply(JsArrayMixed jsArrayMixed) throws OperationException {
-                ConfirmCallback confirmCallback = new ConfirmCallback() {
-                    @Override
-                    public void accepted() {
-                        final String workspaceId = appContext.getWorkspaceId();
-                        workspaceServiceClient.stop(workspaceId).then(new Operation<Void>() {
-                            @Override
-                            public void apply(Void aVoid) throws OperationException {
-                                checkWsStatus(workspaceId);
-                            }
-                        });
-                    }
-                };
+        Promises.all(promises).then(jsArrayMixed -> {
+            ConfirmCallback confirmCallback = () -> {
+                final String workspaceId = appContext.getWorkspaceId();
+                workspaceServiceClient.stop(workspaceId).then(aVoid -> {
+                    checkWsStatus(workspaceId);
+                });
+            };
 
-                ConfirmDialog dialog = dialogFactory.createConfirmDialog(localizationConstants.updateSDKViewTitle(),
-                                                                         localizationConstants.restartViewText(),
-                                                                         localizationConstants.restartViewButtonRestartNow(),
-                                                                         localizationConstants.restartViewButtonRestartLater(),
-                                                                         confirmCallback,
-                                                                         null);
-                dialog.show();
-            }
+            ConfirmDialog dialog = dialogFactory.createConfirmDialog(localizationConstants.updateSDKViewTitle(),
+                                                                     localizationConstants.restartViewText(),
+                                                                     localizationConstants.restartViewButtonRestartNow(),
+                                                                     localizationConstants.restartViewButtonRestartLater(),
+                                                                     confirmCallback,
+                                                                     null);
+            dialog.show();
         });
     }
 
     private void checkWsStatus(final String workspaceId) {
-        workspaceServiceClient.getWorkspace(workspaceId).then(new Operation<WorkspaceDto>() {
-            @Override
-            public void apply(WorkspaceDto workspaceDto) throws OperationException {
-                if (WorkspaceStatus.STOPPED.equals(workspaceDto.getStatus())) {
-                    Window.Location.reload();
-                } else {
-                    new Timer() {
-                        @Override
-                        public void run() {
-                            checkWsStatus(workspaceId);
-                        }
-                    }.schedule(1000);
-                }
+        workspaceServiceClient.getWorkspace(workspaceId).then(workspaceDto -> {
+            if (WorkspaceStatus.STOPPED.equals(workspaceDto.getStatus())) {
+                Window.Location.reload();
+            } else {
+                new Timer() {
+                    @Override
+                    public void run() {
+                        checkWsStatus(workspaceId);
+                    }
+                }.schedule(1000);
             }
         });
     }
 
-    private Promise<String> update(TargetForUpdate target) {
-        final StatusNotification notification = notificationManager.notify(target.getName(),
-                                                                           localizationConstants.updateSDKNotificationUpdatingMessage(),
-                                                                           PROGRESS,
-                                                                           FLOAT_MODE);
-
-        return sdkUpdaterProvider.get().installSDK(target, view.getSelectedVersion()).then(new Operation<String>() {
-            @Override
-            public void apply(String message) throws OperationException {
-                notification.setStatus(StatusNotification.Status.SUCCESS);
-                notification.setContent(message);
-            }
-        }).catchError(new Operation<PromiseError>() {
-            @Override
-            public void apply(PromiseError arg) throws OperationException {
-                notification.setStatus(FAIL);
-                notification.setContent(localizationConstants.updateSDKNotificationUpdateFailMessage() + arg.getMessage());
-            }
+    private Promise<String> update(TargetForUpdate target, StatusNotification notification) {
+        return sdkUpdaterProvider.get().installSDK(target, view.getSelectedVersion()).then(message -> {
+            notification.setStatus(StatusNotification.Status.SUCCESS);
+            notification.setContent(message);
+        }).catchError(arg -> {
+            notification.setStatus(FAIL);
+            notification.setContent(localizationConstants.updateSDKNotificationUpdateFailMessage() + arg.getMessage());
         });
     }
 
     @Override
     public void onCancelClicked() {
         view.close();
+    }
+
+    @Override
+    public void onMachineStatusChanged(MachineStatusChangedEvent machineStatusChangedEvent) {
+        switch (machineStatusChangedEvent.getEventType()) {
+            case DESTROYED:
+                for (StatusNotification notification : notifications) {
+                    if (PROGRESS.equals(notification.getStatus())) {
+                        notification.setStatus(FAIL);
+                        notification.setContent(localizationConstants.updateSDKNotificationUpdateFailMessage() + " device " +
+                                                machineStatusChangedEvent.getMachineName() + " disconnected");
+                    }
+                }
+                break;
+            case RUNNING:
+                break;
+            case ERROR:
+                break;
+        }
     }
 }
